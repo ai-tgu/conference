40c468617aae670b376ad27ea5ada1364ddcd551
ai-tgu <tranhuylong@tgu.edu.vn>
2026-09-14 15:16:07 +0700

    Update data.js

--- 1391/data.js
+++ 1391/data.js
@@ -42,9 +42,12 @@ const CONFERENCE_DATA = {
 
   // 3. DỮ LIỆU VIDEO (KHÔNG GIAN TGU)
   videos: [
-    { id: "vid1", title: "KỶ NIỆM 21 NĂM NGÀY THÀNH LẬP TRƯỜNG ĐẠI HỌC TIỀN GIANG (06/6/2005 - 06/6/2026)", duration: "15:30", embedCode: "https://www.youtube.com/embed/DWNYvUjl6Rc" },
-    { id: "vid2", title: "Thành tựu Chuyển đổi số năm học 2025-2026", duration: "08:15", embedCode: "Mã nhúng Youtube/Iframe 2" },
-    { id: "vid3", title: "Giới thiệu cơ sở vật chất mới", duration: "05:45", embedCode: "Mã nhúng Youtube/Iframe 3" }
+    { 
+        id: "vid1", 
+        title: "KỶ NIỆM 21 NĂM NGÀY THÀNH LẬP TRƯỜNG ĐẠI HỌC TIỀN GIANG (06/6/2005 - 06/6/2026)", 
+        duration: "07:49", 
+        youtubeUrl: "https://www.youtube.com/embed/DWNYvUjl6Rc" 
+      }
   ],
 
   // 4. DỮ LIỆU LỊCH TRÌNH
